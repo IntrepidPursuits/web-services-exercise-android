166a9d3815119d6c257b2dd15a24d2dd3ee4b912
Gabi Scardine Silva <gabiscardinesilva@Gabis-MacBook-Pro.local>
2017-01-11 15:40:54 -0500

    Maintain AsyncTask, but now with retrofit

--- app/src/main/java/io/intrepid/webservicesexercise/GetGitHubUserTask.java
+++ app/src/main/java/io/intrepid/webservicesexercise/GetGitHubUserTask.java
@@ -2,41 +2,60 @@ package io.intrepid.webservicesexercise;
 
 import android.os.AsyncTask;
 
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
-import timber.log.Timber;
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Converter;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 class GetGitHubUserTask extends AsyncTask<String, Void, GitHubUser> {
+    public static final String BASE_URL = "https://api.github.com/";
+    private static final long CONNECTION_TIMEOUT = 15;
+
     private Callback callback;
+
+    private static GithubApi createRestApi() {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        OkHttpClient httpClient = builder
+                .connectTimeout(CONNECTION_TIMEOUT, TimeUnit.SECONDS)
+                .build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(httpClient)
+                .addConverterFactory(getConverter())
+                .build();
+
+        return retrofit.create(GithubApi.class);
+    }
+
+    private static Converter.Factory getConverter() {
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .setPrettyPrinting()
+                .create();
+        return GsonConverterFactory.create(gson);
+    }
 
     @Override
     protected GitHubUser doInBackground(String... params) {
+
         try {
-            URL url = new URL("https://api.github.com/users/" + params[0]);
+            GithubApi api = createRestApi();
 
-            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-            httpURLConnection.setRequestMethod("GET");
+            Call<GitHubUser> call = api.getUser(params[0]);
+            GitHubUser user = call.execute().body();
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
-            String line;
-            StringBuilder response = new StringBuilder();
-            while ((line = reader.readLine()) != null) {
-                response.append(line);
-            }
-            reader.close();
-
-            httpURLConnection.disconnect();
-
-            Gson gson = new Gson();
-            return gson.fromJson(response.toString(), GitHubUser.class);
+            return user;
         } catch (IOException e) {
-            Timber.e(e);
+            e.printStackTrace();
         }
         return null;
     }
