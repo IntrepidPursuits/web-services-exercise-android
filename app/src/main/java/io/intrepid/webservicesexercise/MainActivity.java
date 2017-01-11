a79726c643c47e130324bc8c3e7e04748ad43377
Matt Thompson <mthompson@intrepid.io>
2017-01-11 15:35:24 -0500

    replaced old dumb async task with retrofit

--- app/src/main/java/io/intrepid/webservicesexercise/MainActivity.java
+++ app/src/main/java/io/intrepid/webservicesexercise/MainActivity.java
@@ -7,18 +7,28 @@ import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
+import java.util.concurrent.TimeUnit;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import timber.log.Timber;
 
-public class MainActivity extends AppCompatActivity implements GetGitHubUserTask.Callback {
+public class MainActivity extends AppCompatActivity {
+    private static String GITHUB_BASE_URL = "https://api.github.com/";
     @BindView(R.id.username_input)
     EditText usernameInputView;
     @BindView(R.id.avatar)
     ImageView avatarView;
 
-    private GetGitHubUserTask getGitHubUserTask;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,29 +38,44 @@ public class MainActivity extends AppCompatActivity implements GetGitHubUserTask
         ButterKnife.bind(this);
 
         Timber.plant(new Timber.DebugTree());
+
     }
 
+    private static GitHubApi createRestApi() {
+        OkHttpClient.Builder builder = new OkHttpClient().newBuilder();
+        OkHttpClient httpClient = builder
+                .connectTimeout(2, TimeUnit.SECONDS)
+                .build();
+        return new Retrofit.Builder()
+                .baseUrl(GITHUB_BASE_URL)
+                .client(httpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(GitHubApi.class);
+    }
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
-        if (getGitHubUserTask != null) {
-            getGitHubUserTask.setCallback(null);
-        }
     }
 
     @OnClick(R.id.submit_button)
     public void submitButtonClicked() {
-        getGitHubUserTask = new GetGitHubUserTask();
-        getGitHubUserTask.setCallback(this);
-        getGitHubUserTask.execute(usernameInputView.getText().toString());
-    }
+        Call<GitHubUser> call = createRestApi().getUser(usernameInputView.getText().toString());
+        call.enqueue(new Callback<GitHubUser>() {
+            @Override
+            public void onResponse(Call<GitHubUser> call, Response<GitHubUser> response) {
+                GitHubUser user = response.body();
+                Picasso.with(getApplicationContext())
+                        .load(user.getAvatarUrl())
+                        .fit()
+                        .into(avatarView);
+            }
 
-    @Override
-    public void onResult(GitHubUser gitHubUser) {
-        Picasso.with(this)
-                .load(gitHubUser.getAvatarUrl())
-                .fit()
-                .into(avatarView);
+            @Override
+            public void onFailure(Call<GitHubUser> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
+
     }
 }
