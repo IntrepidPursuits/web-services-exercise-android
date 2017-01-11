d1a6e07c23ae11124582183d57db40f69972c53e
Gabi Scardine Silva <gabiscardinesilva@Gabis-MacBook-Pro.local>
2017-01-11 15:40:30 -0500

    Create an interface for retrofit api methods

--- app/src/main/java/io/intrepid/webservicesexercise/GithubApi.java
+++ app/src/main/java/io/intrepid/webservicesexercise/GithubApi.java
@@ -1,12 +1,10 @@
 package io.intrepid.webservicesexercise;
 
-import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface GithubApi {
     @GET("users/{user}")
-    Call<List<GitHubUser>> listUsers(@Path("user") String user);
+    Call<GitHubUser> getUser(@Path("user") String user);
 }
