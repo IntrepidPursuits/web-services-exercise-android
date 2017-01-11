9ec27c6d8d434e8e204a067dbbbd81ba52551d86
Gabi Scardine Silva <gabiscardinesilva@Gabis-MacBook-Pro.local>
2017-01-11 15:39:39 -0500

    Add retrofit gson converter to gradle

--- app/src/main/java/io/intrepid/webservicesexercise/GithubApi.java
+++ app/src/main/java/io/intrepid/webservicesexercise/GithubApi.java
@@ -1,4 +1,12 @@
 package io.intrepid.webservicesexercise;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public interface GithubApi {
+    @GET("users/{user}")
+    Call<List<GitHubUser>> listUsers(@Path("user") String user);
 }
